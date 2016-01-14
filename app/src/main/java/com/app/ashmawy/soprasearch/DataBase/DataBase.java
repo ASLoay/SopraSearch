31f0b89603b741194f66ac130d10c5e3ea0206f4
Cyril Janting Anak Stell <cyril.janting@gmail.com>
2016-01-14 02:16:52 +0100

    modified databasa.java

--- app/src/main/java/com/app/ashmawy/soprasearch/DataBase/DataBase.java
+++ app/src/main/java/com/app/ashmawy/soprasearch/DataBase/DataBase.java
@@ -164,8 +164,9 @@ public class DataBase extends DataBaseHandler implements DB_Output {
         String query_end = " AND " + ID_ROOM + " NOT IN ("
                 + " SELECT " + ROOM_RES
                 + " FROM " + TABLE_RESERVATIONS
-                + " WHERE ('" + begin + "' BETWEEN " + DATE_BEGIN
-                + " AND " + DATE_END + ")"
+                + " WHERE (('" + begin + "' BETWEEN " + DATE_BEGIN
+                + " AND " + DATE_END + ") AND ('" + end + "' BETWEEN " + DATE_BEGIN
+                + " AND " + DATE_END + "))"
                 +  "));";
         Cursor c = SopraDB.rawQuery(query_begin + particularities + query_end, null);
         c.moveToFirst();
